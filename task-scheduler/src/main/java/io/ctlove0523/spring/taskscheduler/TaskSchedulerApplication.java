ea36fde0e7d83133104eac151f48401a6dcf6e5a
ctlove0523 <478309639@qq.com>
2018-12-14 11:40:34 +0800

    增加定时任务相关的

---
+++ task-scheduler/src/main/java/io/ctlove0523/spring/taskscheduler/TaskSchedulerApplication.java
@@ -0,0 +1,14 @@
+package io.ctlove0523.spring.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerApplication.class, args);
+	}
+
+}
+
