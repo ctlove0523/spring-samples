39eb25223ddc977ba658d80d4eae17011dba1ee5
ctlove0523 <478309639@qq.com>
2020-03-08 00:26:28 +0800

    #1 增加interceptor

---
+++ interceptor/src/main/java/io/ctlove0523/spring/interceptor/InterceptorApplication.java
@@ -0,0 +1,15 @@
+package io.ctlove0523.spring.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chentong
+ */
+@SpringBootApplication
+public class InterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterceptorApplication.class, args);
+    }
+}
