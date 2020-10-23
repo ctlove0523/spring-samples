49b987af5e8b59419d1ef2a77f62af3b82043254
ctlove0523 <478309639@qq.com>
2020-10-23 10:06:24 +0800

    Update StorageRouteDefinitionRepository.java

--- spring-cloud-gateway/src/main/java/io/ctlove0523/spring/gateway/routes/StorageRouteDefinitionRepository.java
+++ spring-cloud-gateway/src/main/java/io/ctlove0523/spring/gateway/routes/StorageRouteDefinitionRepository.java
@@ -25,6 +25,41 @@ public class StorageRouteDefinitionRepository implements RouteDefinitionReposito
 
     @Autowired
     private DbRouteDefinitionRepository repository;
+    static {
+        RouteDefinition definition = new RouteDefinition();
+        definition.setId("chen-tong");
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://127.0.0.1:8888/header").build().toUri();
+        definition.setUri(uri);
+
+        //定义第一个断言
+        PredicateDefinition predicate = new PredicateDefinition();
+        predicate.setName("Path");
+
+        Map<String, String> predicateParams = new HashMap<>(8);
+        predicateParams.put("pattern", "/jd");
+        predicate.setArgs(predicateParams);
+
+        //定义Filter
+        FilterDefinition filter = new FilterDefinition();
+        filter.setName("AddRequestHeader");
+        Map<String, String> filterParams = new HashMap<>(8);
+        //该_genkey_前缀是固定的，见org.springframework.cloud.gateway.support.NameUtils类
+        filterParams.put("_genkey_0", "header");
+        filterParams.put("_genkey_1", "addHeader");
+        filter.setArgs(filterParams);
+
+        FilterDefinition filter1 = new FilterDefinition();
+        filter1.setName("AddRequestParameter");
+        Map<String, String> filter1Params = new HashMap<>(8);
+        filter1Params.put("_genkey_0", "param");
+        filter1Params.put("_genkey_1", "addParam");
+        filter1.setArgs(filter1Params);
+
+        definition.setFilters(Arrays.asList(filter, filter1));
+        definition.setPredicates(Collections.singletonList(predicate));
+
+        routes.put(definition.getId(), definition);
+    }
 
     @Override
     public Flux<RouteDefinition> getRouteDefinitions() {
