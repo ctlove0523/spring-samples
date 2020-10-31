81d920116b2d42ef966f0c0fe4b1d9bb7c1aa2da
ctlove0523 <478309639@qq.com>
2020-11-01 00:33:32 +0800

    初始化工程

---
+++ spring-cloud-consul/src/main/java/io/github/ctlove0523/consul/ConsulApplication.java
@@ -0,0 +1,11 @@
+package io.github.ctlove0523.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulApplication.class, args);
+	}
+}
