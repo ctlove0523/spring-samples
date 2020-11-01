1bd6105fac759c8da8eb9806ccf6f905acfe372c
ctlove0523 <478309639@qq.com>
2020-11-01 23:11:54 +0800

    change project name

---
+++ spring-cloud-consul/src/main/java/io/github/ctlove0523/consul/health/HealthController.java
@@ -0,0 +1,17 @@
+package io.github.ctlove0523.consul.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HealthController {
+
+	@RequestMapping(value = "/health",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> check() {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
